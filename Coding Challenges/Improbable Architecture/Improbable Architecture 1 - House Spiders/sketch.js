0e67858df63757b8fda37dee6e10e3864365975e
Project Somedays <projectsomedays@gmail.com>
2023-12-17 11:19:45 +1100

    Legs now in sync

--- Coding Challenges/Improbable Architecture/Improbable Architecture 1 - House Spiders/sketch.js
+++ Coding Challenges/Improbable Architecture/Improbable Architecture 1 - House Spiders/sketch.js
@@ -30,30 +30,26 @@ function draw(){
   yPos = height/2 + yPosMag*sin(aOffset/2);
   
   line(width/2, yPos,width/2, 0);
-  for(let i = 0; i < 8; i++){
-    let a;
+  for(let i = 0; i < 4; i++){
+    let a = -PI/6 + i * HALF_PI/4;
     let phase = i*0.1*PI;
-    if(i < 4){
-      a = -PI/6 + i * HALF_PI/4;
-    } else {
-      a = -PI/6 + (i-4) * HALF_PI/4;
+    drawLeg(width/2, yPos, a + 0.2*sin(aOffset + phase), false);
+    drawLeg(width/2, yPos, a + 0.2*sin(aOffset + phase), true);
     }
-    isFlipped = i > 3;
-    drawLeg(width/2, yPos, a + 0.2*sin(aOffset + phase), isFlipped);
-  }
-  // drawLeg(width/2, height/2,0);
+  
+    // drawLeg(width/2, height/2,0);
   image(houseImg, width/2 - houseImg.width/2, yPos- 2*houseImg.height/3);
   
   aOffset += legWiggleRate;
-}
+    
+  }
+  
 
 function drawLeg(x,y,a, isFlipped){
   push();
   translate(x,y);
   if(isFlipped) scale(-1,1);
   rotate(a);
-  
-  
   image(legTipImg, 100 + legBaseImg.width - 30 - legTipImg.width/2, - legBaseImg.height/2);
   image(legBaseImg, 100 - legBaseImg.width/2, - legBaseImg.height/2, legBaseImg.width, legBaseImg.height);
   pop();
