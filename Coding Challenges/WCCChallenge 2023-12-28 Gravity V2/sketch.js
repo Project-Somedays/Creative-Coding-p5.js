279e6798d866c4525bdcdf85109df0af1bd26628
Project Somedays <projectsomedays@gmail.com>
2023-12-28 23:51:19 +1100

    Oh I LOVE this...

--- Coding Challenges/WCCChallenge 2023-12-28 Gravity V2/sketch.js
+++ Coding Challenges/WCCChallenge 2023-12-28 Gravity V2/sketch.js
@@ -9,13 +9,18 @@ Inspired by The Expanse
 And Gravity Sims which are fun
 */
 
-const predictSteps = 100;
-const velStep = 0.5;
+const predictSteps = 10000;
+const velStep = 1;
 const velMax = 5;
-const traceSize = 5;
+const traceSize = 2;
+const G = 40;
+const colours = ["#d9ed92","#b5e48c","#99d98c","#76c893","#52b69a","#34a0a4","#168aad","#1a759f","#1e6091","#184e77"];
+let sunSize;
 let c;
 let m;
 let velocities = [];
+let traceShower;
+
 
 
 function setup() {
@@ -23,23 +28,29 @@ function setup() {
   m = createVector(mouseX, mouseY);
   c = createVector(width/2, height/2);
   velocities = [];
-  for(let i = 0; i < velMax; i += velStep){
+  for(let i = 0; i < colours.length; i += velStep){
     velocities.push(createVector(i,0));
   }
+  sunSize = width/20;
+  noStroke();
+  
 }
 
 function draw() {
   background(0);
   m.set(mouseX, mouseY);
+  traceShower = new TraceShower(m.x, m.y);
+  traceShower.update();
+  traceShower.show();
   
   fill(255, 255, 0);
-  circle(c.x, c.y, width/20);
+  circle(c.x, c.y, sunSize);
 
   fill(255);
   circle(m.x, m.y, 5);
 }
 
-class Body{
+class TraceShower{
   constructor(x,y){
     this.p = createVector(x,y);
     this.traces = [];
@@ -50,24 +61,47 @@ class Body{
   }
 
   update(){
-    for(let i = 0; i < velocities.length; i++){
+    for(let i = 0; i < colours.length; i++){
       let trace = [];
       let p = createVector(this.p.x, this.p.y); // copies of the start
       let v = createVector(velocities[i].x, velocities[i].y);
       for(let j = 0; j < predictSteps; j++){
-        
+        let attractionF = p5.Vector.sub(c, p); // direction of acceleration
+        let p2c = p5.Vector.dist(p, c); // get the distance from the body
+        if(p2c < sunSize/2 || p2c > 1.5*max(width, height)) break; // if we've crashed or if we've 
+        attractionF.setMag(G/p2c); // set the mag inversely proportional to the distance
+        v.add(attractionF); // change the acceleration
+        p.add(v); // change the position
+        trace.push(createVector(p.x, p.y)); // add the position to the trace
       }
+      this.traces.push(new Trace(colours[i], trace));
     }
-    this.traces.push(trace);
+     // add the trace
 
   }
 
   show(){
     for(let trace of this.traces){
-      for(let p of trace){
-        circle(p.x, p.y, traceSize)
-      }
+      trace.show()
     }
 
   }
 }
+
+class Trace{
+  constructor(col, traceArray){
+    this.col = col;
+    this.traceArray = traceArray;
+  }
+
+  show(){
+    stroke(this.col);
+    noFill();
+    beginShape();
+    for(let t of this.traceArray){
+      vertex(t.x, t.y);
+    }
+    endShape(OPEN);
+  }
+}
+
