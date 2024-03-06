1b17fd0a734d0aabe17de85e76e9c2bf4b9692f8
Project Somedays <projectsomedays@gmail.com>
2024-03-06 22:17:32 +1100

    Progress! Now just need to filter out transparency

--- WCCChallenge 2024-03-04 Firefly/sketch.js
+++ WCCChallenge 2024-03-04 Firefly/sketch.js
@@ -13,20 +13,68 @@ let sampleCount = 1000;
 let swarm;
 let neighbourhood;
 const swarmProgressRate = 0.01;
+let img;
+let darkestYellow; 
+let yellow;
+let f;
+
+function preload(){
+  img = loadImage("Nathan Fillion.png");
+}
 
 function setup() {
   createCanvas(1080, 1080, P2D);
+  darkestYellow = color(139, 128, 0);
+  yellow = color(255, 255, 0);
   neighbourhood = 0.25*max(width, height);
   swarm = new Swarm(sampleCount);
   console.log(swarm.swarm.length);
   noStroke();
   frameRate(30);
+  img = convertToFireflyColours(img);
+  f = convertImageToField(img, 10);
+  
 }
 
 function draw() {
   background(0);
-  swarm.update();
-  swarm.show();
+  // image(img, 0, 0);
+  // swarm.update();
+  // swarm.show();
+  for(let fObj of f){
+    fill(fObj.col);
+    circle(fObj.p.x, fObj.p.y, 10);
+  }
 
   globOffset += swarmProgressRate;
+}
+
+function convertToFireflyColours(imageToConvert){
+  // shift pixels in img to yellow colour space
+  imageToConvert.loadPixels();
+  for(let y = 0; y < imageToConvert.height; y++){
+    for(let x = 0; x < imageToConvert.width; x++){
+      let ix = (x + y*imageToConvert.width)*4;
+      let brightness = int((imageToConvert.pixels[ix] + imageToConvert.pixels[ix+1] + imageToConvert.pixels[ix+2])/3);
+      let cVal = map(brightness, 0, 255, 0, 1);
+      let c = lerpColor(darkestYellow, yellow, cVal);
+      imageToConvert.pixels[ix] = red(c);
+      imageToConvert.pixels[ix + 1] = green(c)
+      imageToConvert.pixels[ix + 2] = blue(c);
+    }
+  }
+  imageToConvert.updatePixels();
+  return imageToConvert;
+}
+
+function convertImageToField(imageToConvert, sampleEvery){
+  field = [];
+  for(let y = 0; y < imageToConvert.height; y+= sampleEvery){
+    for(let x = 0; x < imageToConvert.width; x+= sampleEvery){
+      let ix = (x + y*imageToConvert.width)*4;
+      let c = color(imageToConvert.pixels[ix], imageToConvert.pixels[ix+1], imageToConvert.pixels[ix+2]);
+      field.push({p: createVector(x,y), col: c});
+    }
+  }
+  return field;
 }
