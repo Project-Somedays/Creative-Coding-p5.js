c151790174077e097a91a8eba0fad2aa4ffac73b
Project Somedays <projectsomedays@gmail.com>
2024-03-11 15:11:43 +1100

    Starting WCCChallenge Week 11 - Swiss Design

--- WCCChallenge 2024 Week 11 Swiss Design/sketch.js
+++ WCCChallenge 2024 Week 11 Swiss Design/sketch.js
@@ -1,3 +1,15 @@
+/*
+Author: Project Somedays
+Date: 2024-03-11
+Title: #WCCChallenge - Swiss Design, hosted by @sableraph on Twitch.tv: https://www.twitch.tv/sableraph
+
+Bauhaus design looks delightfully clean and simple to produce.
+To put my own twist on it, I want to poke Walter Gropius, founder of the movement, for more ideas.
+Turns out the wikipedia picture ALREADY has him in Charles Xavier power pose for ideas.
+Also a good opportunity to learn about masking in p5.js
+*/
+
+
 function setup() {
   createCanvas(400, 400);
 }
