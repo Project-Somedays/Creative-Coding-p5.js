43d69fe8186dd0eab75969b6a69617e52a34227a
Project Somedays <projectsomedays@gmail.com>
2024-03-12 16:58:09 +1100

    Well giving up for today

--- Genuary 2024 Day 15 - Physics Library/sketch.js
+++ Genuary 2024 Day 15 - Physics Library/sketch.js
@@ -6,31 +6,64 @@ Title: Physics Library
 Going for an underwater reef feel, spawning on a noise field, with the current ebbing and flowing
 */
 const zoomFactor = 400;
-const everyNthPixel = 5;
+const everyNthPixel = 10;
+const segPerFrond = 5;
+const lineLength = 40;
+const segL = lineLength/segPerFrond;
+const tideCycleFrames = 600;
+
+
 
 
 // module aliases
-var Engine = Matter.Engine,
-    Render = Matter.Render,
-    Runner = Matter.Runner,
-    Bodies = Matter.Bodies,
-    Composite = Matter.Composite,
-    Composites = Matter.Composites;
+const {
+  Engine,
+  World,
+  Bodies,
+  Body,
+  Composite,
+  Constraint,
+  Runner
+} = Matter;
 
 let engine;
 let world;
-let tide;
-let lineLength = 200;
+let tideForce;
+let tideOffset = 0;
+let tideTracker = 0;
 
-
+let testFrond;
+let allFronds = [];
 
 
 
 function setup() {
-  createCanvas(1080, 1920);
+  createCanvas(720, 720);
   // create an engine
-  // engine = Engine.create();
-  // world = engine.world;
+  engine = Engine.create();
+  
+  world = engine.world;
+  world.gravity.y = 0.9;
+  noStroke();
+  
+  tideForce = createVector(0,0);
+
+  testFrond =  new Frond(width/2, height/8);
+  Runner.run(engine);
+
+  for(let y = 0; y < height; y+=everyNthPixel){
+    for(let x = 0; x < width; x+=everyNthPixel){
+      let nVal = noise(x/zoomFactor,y/zoomFactor);
+      if(nVal > 0.5 && nVal < 0.51){
+        allFronds.push(new Frond(x,y));
+        
+        
+        // circle(x,y, everyNthPixel);
+      }
+    }
+  }
+ 
+
 
   // // create two boxes and a ground
   // var boxA = Bodies.rectangle(400, 200, 80, 80);
@@ -49,21 +82,25 @@ function setup() {
 
 function draw() {
   background('#0D1C35');
-  // tidePhase = 0.5*(sin(radians(10*frameCount)) + 1); // goes between 0 and 1
-  tide = map(noise(frameCount/200),0,1, 0, PI);
-  for(let y = 0; y < height; y+=everyNthPixel){
-    for(let x = 0; x < width; x+=everyNthPixel){
-      let nVal = noise(x/zoomFactor,y/zoomFactor);
-      if(nVal > 0.5 && nVal < 0.51){
-        let offset = map(x, 0, width, 0, HALF_PI);
-        fill(255);
-        stroke(255);
-        strokeWeight(2);
-        line(x,y,x + lineLength*cos(tide + offset),y + lineLength*sin(tide + offset));
-        // circle(x,y, everyNthPixel);
-      }
-    }
+  fill(255);
+  textSize(50);
+  textAlign(CENTER, CENTER);
+  tideForce.x = 0.5*(sin(tideTracker) + map(noise(tideOffset), 0, 1, -0.5, 0.5));
+  text(tideForce.x, width/2, 0.1*height);
+  world.gravity.x = tideForce.x;
+  world.gravity.y = 0.9;
+  Runner.run(engine);
+  
+
+  for(let i = 0; i < allFronds.length; i++){
+    allFronds[i].show();
   }
+  // testFrond.applyForce(tideForce);
+  // tidePhase = 0.5*(sin(radians(10*frameCount)) + 1); // goes between 0 and 1
+  // tide = map(noise(frameCount/200),0,1, 0, PI);
+  
+  tideTracker += TWO_PI/tideCycleFrames;
+  tideOffset += 0.001;
 
 }
 
