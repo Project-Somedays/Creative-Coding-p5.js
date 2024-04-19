6349e2e96bf8d16e8227b9ef4c851017f110724c
Project Somedays <projectsomedays@gmail.com>
2024-04-19 11:52:04 +1000

    Decided to combine sandbox mode and animation mode

--- WCCChallenge 2024 Week 16 Feedback/sketch.js
+++ WCCChallenge 2024 Week 16 Feedback/sketch.js
@@ -15,23 +15,23 @@ Drawing the canvas back to itself on a doppler spiral
 Doppler spiral function comes from Wolfram Alpha: https://mathworld.wolfram.com/DopplerSpiral.html
 */
 
-const dopplerSpiral = (a,b,t) => createVector(a*(b * t + t * cos(t)), a * t * sin(t));
+const dopplerSpiral = (a, b, t) => createVector(a * (b * t + t * cos(t)), a * t * sin(t));
 const noiseProgRate = 200;
 const colourProgRate = 0.2;
 let dopplerA, dopplerB;
 let xOff, yOff, aOff;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(1920, 1080);
   noFill();
   rectMode(CENTER);
   imageMode(CENTER);
-  colorMode(HSB,360, 100, 100, 100);
+  colorMode(HSB, 360, 100, 100, 100);
   pixelDensity(1);
-  
+
   strokeWeight(20);
   background(0);
-  dopplerA = random(0.1,2);
+  dopplerA = random(0.1, 2);
   dopplerB = random(0.01, 0.1);
   xOff = random(1000);
   yOff = random(1000);
@@ -40,18 +40,18 @@ function setup() {
 
 function draw() {
   let g = get();
-  let t = 0.5*(sin(radians(frameCount/10))+1)*10*TWO_PI;
-  let p = dopplerSpiral(dopplerA,dopplerB,t);
-  let x = map(noise(frameCount/noiseProgRate + xOff), 0, 1, 0.01*width, 0.1*width);
-  let y = map(noise(frameCount/noiseProgRate + yOff), 0, 1, 0.01*width, 0.1*width);
-  let a = map(noise(frameCount/noiseProgRate + aOff), 0, 1, -TWO_PI, TWO_PI);
-  stroke(colourProgRate*frameCount%360,100, 100, 25);
- 
+  let t = 0.5 * (sin(radians(frameCount / 10)) + 1) * 10 * TWO_PI;
+  let p = dopplerSpiral(dopplerA, dopplerB, t);
+  let x = map(noise(frameCount / noiseProgRate + xOff), 0, 1, 0.01 * width, 0.1 * width);
+  let y = map(noise(frameCount / noiseProgRate + yOff), 0, 1, 0.01 * width, 0.1 * width);
+  let a = map(noise(frameCount / noiseProgRate + aOff), 0, 1, -TWO_PI, TWO_PI);
+  stroke(colourProgRate * frameCount % 360, 100, 100, 25);
+
   push();
-  translate(width/2, height/2);
-  image(g, p.x/2, p.y/2, width*0.99, height*0.99);
+  translate(width / 2, height / 2);
+  image(g, p.x / 2, p.y / 2, width * 0.99, height * 0.99);
   rotate(a);
-  rect(0,0, x,y, 0.1*x, 0.1*x);
+  rect(0, 0, x, y, 0.1 * x, 0.1 * x);
   pop();
-  
+
 }
