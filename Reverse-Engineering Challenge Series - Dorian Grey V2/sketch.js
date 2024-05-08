7df33d04d9c088989dd4dbe451024b13561ee151
Project Somedays <projectsomedays@gmail.com>
2024-05-09 08:18:21 +1000

    Final

--- Reverse-Engineering Challenge Series - Dorian Grey V2/sketch.js
+++ Reverse-Engineering Challenge Series - Dorian Grey V2/sketch.js
@@ -1,25 +1,40 @@
+/* 
+Author: Project Somedays
+Date: 2024-05-08
+Title: Reverse-Engineering Challenge Series - The Picture of Dorian Grey
 
-let portrait, dorian, bgImage;
-// let portraitPixelArr, dorianPixelArr;
+Saw a cool thing from @tim_rodenbroeker and that sent me down a rabbit hole of sampling from different images: https://www.instagram.com/p/C6q8n93uJLt/
+The Picture of Dorian Grey seemed like the perfect subject matter to either sample from the Picture or Dorian himself.
 
-let captureMode = true;
+In this version, I'm using a 2D perlin noise field that progresses over time, deciding which sample to draw
+
+Lends itself to something with a shader if anyone wants to make a fork and run it on the GPU. It's on my list to learn still...
+
+Original Image: https://s26162.pcdn.co/wp-content/uploads/sites/2/2021/07/Dorian-Gray.jpg
+
+*/
+
+
+let picture, dorian, bgImage;
+let portraitLayer, dorianLayer;
+let opacityThreshold = 5;
+
+let step = 10;
+const noiseZoom = 300;
+const noiseProgRate = 0.01;
 
 let tiles = [];
-let cycleFrames = 120;
-let n = 500;
-let step = 5;
-let noiseZoom = 300;
-let noiseProgRate = 0.01;
-let dl;
 
-let portraitLayer, dorianLayer;
 
+// ---- CAPTURE BIZ ------ //
 let fps = 30;
 let capturer;
+const captureMode = false;
 
 
-function preload(){
-  portrait = loadImage("Portrait.png");
+
+function preload() {
+  picture = loadImage("Picture.png");
   dorian = loadImage("Dorian.png");
   bgImage = loadImage("Background.png");
 }
@@ -30,31 +45,24 @@ function setup() {
   frameRate(30);
 
   // so we can just grab the pixels that have something on them
-
   portraitLayer = createGraphics(width, height);
-  portraitLayer.image(portrait, 0,0);
+  portraitLayer.image(picture, 0, 0);
   dorianLayer = createGraphics(width, height);
-  dorianLayer.image(dorian,0,0);
+  dorianLayer.image(dorian, 0, 0);
   dorianLayer.loadPixels();
   portraitLayer.loadPixels();
 
-  // make buffer layers and load the pixels
-  for(let i = 0; i < width; i+= step){
-    for(let j = 0; j < height; j+= step){
-      if(containsNoOpaquePixels(i, j)) tiles.push(new Tile(i, j, step, step));
-      
+  pixelDensity(1);
+
+  // ignore samples that are completely transparent
+  for (let i = 0; i < width; i += step) {
+    for (let j = 0; j < height; j += step) {
+      if (containsOpaquePixels(i, j)) tiles.push(new Tile(i, j));
     }
   }
 
-  console.log(tiles.length);
 
-  // dl = createGraphics(width, height);
-
-  pixelDensity(1);
-
-  // dl.loadPixels();
- 
-
+  // -------------- CAPTURE BIZ ----------------------//
 
   capturer = new CCapture({
     format: 'png',
@@ -62,27 +70,27 @@ function setup() {
   });
   stroke(255);
   noFill();
-  image(bgImage, 0, 0);
+
 }
 
 function draw() {
-  
-  if(captureMode && frameCount === 1) capturer.start();
+  image(bgImage, 0, 0);
+  if (captureMode && frameCount === 1) capturer.start();
 
-  for(let i = 0; i < width; i += step){
-    for(let j = 0; j < height; j += step){
-      let noiseVal = noise(i/noiseZoom, j/noiseZoom, frameCount*noiseProgRate);
-      if(noiseVal < 0.5){
-        image(dorian,i, j, step, step, i, j, step, step) 
-      } else{
-        image(portrait, i, j, step, step, i, j, step, step);
+  for (let i = 0; i < width; i += step) {
+    for (let j = 0; j < height; j += step) {
+      let noiseVal = noise(i / noiseZoom, j / noiseZoom, frameCount * noiseProgRate);
+      if (noiseVal < 0.5) {
+        image(dorian, i, j, step, step, i, j, step, step)
+      } else {
+        image(picture, i, j, step, step, i, j, step, step);
       }
     }
   }
-  
-  
-  
-  if (captureMode && frameCount === 30*30) {
+
+
+
+  if (captureMode && frameCount === 30 * 30) {
     noLoop();
     console.log('finished recording.');
     capturer.stop();
@@ -90,33 +98,37 @@ function draw() {
     return;
   }
 
-  if(captureMode) capturer.capture(document.getElementById('defaultCanvas0'));
-  
+  if (captureMode) capturer.capture(document.getElementById('defaultCanvas0'));
+
 }
 
-function containsNoOpaquePixels(x, y){
-  for(let i = x; i <= x+step; i++){
-    for(let j = y; j <= y + step; j++){
-      let ix = i + j*width;
-      if(portraitLayer.pixels[ix+3] > 0 || dorianLayer.pixels[ix+3] > 0) return true;
+// for filtering out completely transparent pixels that don't change
+function containsOpaquePixels(x, y) {
+  for (let i = x; i <= x + step; i++) {
+    for (let j = y; j <= y + step; j++) {
+      let ix = i + j * width;
+      if (portraitLayer.pixels[ix + 3] > opacityThreshold || dorianLayer.pixels[ix + 3] > opacityThreshold) return true;
     }
   }
   return false;
 }
 
-class Tile{
-  constructor(x,y){
-    this.p = createVector(x,y);
+class Tile {
+  constructor(x, y) {
+    this.p = createVector(x, y);
     this.noiseVal;
   }
-  
-  update(){
-    this.noiseVal = noise(this.p.x/noiseZoom, this.p.y/noiseZoom, frameCount*noiseProgRate);
-  
+
+  update() {
+    this.noiseVal = noise(this.p.x / noiseZoom, this.p.y / noiseZoom, frameCount * noiseProgRate);
   }
 
-  show(){
-    
+  show() {
+    if (this.noiseVal < 0.5) {
+      image(dorian, this.p.x, this.p.y, step, step, this.p.x, this.p.y, step, step);
+    } else {
+      image(picture, this.p.x, this.p.y, step, step, this.p.x, this.p.y, step, step);
+    }
 
   }
 }
