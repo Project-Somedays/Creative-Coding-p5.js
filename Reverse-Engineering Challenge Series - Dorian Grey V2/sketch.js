ff7cbe3bb66d85f3f7a58727cb25ec02c3f4ee2c
Project Somedays <projectsomedays@gmail.com>
2024-05-08 18:43:43 +1000

    That's more than enough time on THAT

--- Reverse-Engineering Challenge Series - Dorian Grey V2/sketch.js
+++ Reverse-Engineering Challenge Series - Dorian Grey V2/sketch.js
@@ -1,5 +1,6 @@
 
-let portrait, dorian;
+let portrait, dorian, bgImage;
+// let portraitPixelArr, dorianPixelArr;
 
 let captureMode = true;
 
@@ -9,23 +10,51 @@ let n = 500;
 let step = 5;
 let noiseZoom = 300;
 let noiseProgRate = 0.01;
+let dl;
+
+let portraitLayer, dorianLayer;
 
 let fps = 30;
 let capturer;
 
+
 function preload(){
   portrait = loadImage("Portrait.png");
   dorian = loadImage("Dorian.png");
+  bgImage = loadImage("Background.png");
 }
 
 
 function setup() {
   createCanvas(1080, 1080);
+  frameRate(30);
+
+  // so we can just grab the pixels that have something on them
+
+  portraitLayer = createGraphics(width, height);
+  portraitLayer.image(portrait, 0,0);
+  dorianLayer = createGraphics(width, height);
+  dorianLayer.image(dorian,0,0);
+  dorianLayer.loadPixels();
+  portraitLayer.loadPixels();
+
+  // make buffer layers and load the pixels
   for(let i = 0; i < width; i+= step){
     for(let j = 0; j < height; j+= step){
-      tiles.push(new Tile(i, j, step, step));
+      if(containsNoOpaquePixels(i, j)) tiles.push(new Tile(i, j, step, step));
+      
     }
   }
+
+  console.log(tiles.length);
+
+  // dl = createGraphics(width, height);
+
+  pixelDensity(1);
+
+  // dl.loadPixels();
+ 
+
 
   capturer = new CCapture({
     format: 'png',
@@ -33,18 +62,27 @@ function setup() {
   });
   stroke(255);
   noFill();
+  image(bgImage, 0, 0);
 }
 
 function draw() {
-  background(0);
+  
   if(captureMode && frameCount === 1) capturer.start();
 
-  for(let t of tiles){
-    t.update();
-    t.show();
+  for(let i = 0; i < width; i += step){
+    for(let j = 0; j < height; j += step){
+      let noiseVal = noise(i/noiseZoom, j/noiseZoom, frameCount*noiseProgRate);
+      if(noiseVal < 0.5){
+        image(dorian,i, j, step, step, i, j, step, step) 
+      } else{
+        image(portrait, i, j, step, step, i, j, step, step);
+      }
+    }
   }
   
-  if (captureMode && frameCount === 30*40) {
+  
+  
+  if (captureMode && frameCount === 30*30) {
     noLoop();
     console.log('finished recording.');
     capturer.stop();
@@ -56,12 +94,19 @@ function draw() {
   
 }
 
+function containsNoOpaquePixels(x, y){
+  for(let i = x; i <= x+step; i++){
+    for(let j = y; j <= y + step; j++){
+      let ix = i + j*width;
+      if(portraitLayer.pixels[ix+3] > 0 || dorianLayer.pixels[ix+3] > 0) return true;
+    }
+  }
+  return false;
+}
 
 class Tile{
-  constructor(x,y,ix, iy){
+  constructor(x,y){
     this.p = createVector(x,y);
-    this.ix = ix;
-    this.iy = iy;
     this.noiseVal;
   }
   
@@ -71,11 +116,36 @@ class Tile{
   }
 
   show(){
-    if(this.noiseVal < 0.5){
-      image(dorian,this.p.x, this.p.y, this.ix, this.iy,this.p.x, this.p.y, this.ix, this.iy) 
-    } else{
-      image(portrait,this.p.x, this.p.y, this.ix, this.iy,this.p.x, this.p.y, this.ix, this.iy);
-    };
+    
 
   }
 }
+
+
+
+
+// pixel-based approach experiment
+
+// portraitLayer = createGraphics(width, height);
+// portraitLayer.image(portrait, 0,0);
+// dorianLayer = createGraphics(width, height);
+// dorianLayer.image(dorian,0,0);
+// dorianLayer.loadPixels();
+// for(let i = 0; i < width; i++){
+//   for(let j = 0; j < height; j++){
+//     let ix = i + width*j;
+//     if(noise(i/noiseZoom,j/noiseZoom, frameCount*noiseProgRate) < 0.5){
+//       dl.pixels[ix] = portraitLayer.pixels[ix]
+//       dl.pixels[ix+1] = portraitLayer.pixels[ix+1]
+//       dl.pixels[ix+2] = portraitLayer.pixels[ix+2]
+//       dl.pixels[ix+3] = portraitLayer.pixels[ix+3]
+//     } else{
+//       dl.pixels[ix] = dorianLayer.pixels[ix]
+//       dl.pixels[ix+1] = dorianLayer.pixels[ix+1]
+//       dl.pixels[ix+2] = dorianLayer.pixels[ix+2]
+//       dl.pixels[ix+3] = dorianLayer.pixels[ix+3]
+//     }
+//   }
+// }
+// dl.updatePixels();
+// image(dl, 0, 0);
