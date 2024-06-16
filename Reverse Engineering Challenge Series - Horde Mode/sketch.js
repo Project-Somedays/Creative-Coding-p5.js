8a1ac11e8ae7a332371ebd2eb04fc336eaa83bcb
Project Somedays <projectsomedays@gmail.com>
2024-06-16 21:46:01 +1000

    Player can now fire gun!

--- Reverse Engineering Challenge Series - Horde Mode/sketch.js
+++ Reverse Engineering Challenge Series - Horde Mode/sketch.js
@@ -1,40 +1,90 @@
+let bullets = [];
+let player;
+let pistolFlintLock;
+
 function setup() {
   createCanvas(min(windowWidth,windowHeight), min(windowWidth, windowHeight));
+  frameRate(30);
+  pistolFlintLock = new Gun("Flintlock Pistol", 60, 0, windowHeight/75);
+  player = new Player(pistolFlintLock);
 }
 
 function draw() {
-  background(220);
+  background(0);
+  player.update()
+  player.show();
+  
+  handleBullets();
+
+  
+  
+  
+
 }
+
+function handleBullets(){
+  for(let i = bullets.length - 1; i >= 0; i--){
+    bullets[i].update();
+    bullets[i].show();
+    if(bullets[i].isSpent) bullets.splice(i,1);
+  }
+}
+
 
 
 class Bullet{
   constructor(x,y,dir,bulletSpeed){
     this.p = createVector(x,y);
     this.dir = createVector(dir.x, dir.y).setMag(bulletSpeed);
+    this.isSpent = false;
   }
   update(){
     this.p.add(this.dir);
-
+    if(this.p.x < 0 || this.p.x > width || this.p.y < 0) this.isSpent = true;
   }
+
+  show(){
+    fill(255);
+    circle(this.p.x, this.p.y, width/50);
+  }
+
+
 }
 
 class Gun{
-  constructor(name, fireRate, spread){
+  constructor(name, fireRate, spread, bulletSpeed){
     this.name = name;
     this.fireRate = fireRate;
     this.spread = spread;
+    this.bulletSpeed = bulletSpeed;
+  }
+
+  fire(x, y){
+    if(frameCount % this.fireRate === 0){
+      let dir = p5.Vector.fromAngle(random(-this.spread/2, this.spread/2) - HALF_PI) ;
+      let bullet = new Bullet(x,y, dir, this.bulletSpeed);
+      bullets.push(bullet);
+    }
   }
   
 }
 
 class Player{
-  constructor(){
+  constructor(gun){
     this.p = createVector(width/2, height*0.9);
     this.health = 100;
-    
-    this.gun = 
-    this.fireSpread = 0;
+    this.gun = gun;
     this.groupSize = 1;
+  }
+
+  update(){
+    this.p.x = mouseX;
+    this.gun.fire(this.p.x, this.p.y);
+  }
+
+  show(){
+    fill(0,255,0);
+    circle(this.p.x, this.p.y, width/20);
   }
 
   
@@ -52,5 +102,5 @@ class Enemy{
     this.p.y += this.speed;
   }
 
-  this.headt
+  
 }
