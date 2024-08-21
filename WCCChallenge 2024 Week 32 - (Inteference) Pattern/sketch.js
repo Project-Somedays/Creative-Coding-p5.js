4e2cac2fa30aa0fa17474a2f1f45e3488c348164
Project Somedays <projectsomedays@gmail.com>
2024-08-21 17:50:35 +1000

    Catchup

--- WCCChallenge 2024 Week 32 - (Inteference) Pattern/sketch.js
+++ WCCChallenge 2024 Week 32 - (Inteference) Pattern/sketch.js
@@ -1,65 +1,132 @@
 /*
 Author: Project Somedays
 Date: 2024-08-10
-Title: WCCChallenge 2024 Week 32 - Pattern
+Title: WCCChallenge 2024 Week 32 - (Inteference) Pattern
 
 Made for Sableraph's weekly creative coding challenges, reviewed weekly on https://www.twitch.tv/sableraph
 See other submissions here: https://openprocessing.org/curation/78544
 Join The Birb's Nest Discord community! https://discord.gg/g5J6Ajx9Am
 
+Have been playing around a lot this week on interference patterns, mostly on the outside of spheres: https://openprocessing.org/sketch/2325058
+Thought I'd flatten it out so you can really see what's happening!
+
+HOW DOES IT WORK? 
+  - Field made of spheres
+  - Distance mapped to each control point 0 to TWO_PI
+Get the value of cosine of that angle and add it to the sphere size
+Sometimes the radius is negative, sometimes positive.
+The superposition of two positives or negatives is a mega sphere
+The superposition of opposite signs cancels out.
+Physics/Maths brain VERY satisfied.
+
+TLDR: Interference patterns 💞
+
+SUGGESTED EXPERIMENTS
+Start at 1 control point and work up
+Also try the decaying cosine function. Note: you'll have to ramp up the amplitude
+Play around the with the phase also
 
 */
 
 let pts = [];
 let controlPoints = [];
 let gui, params;
+let distanceMappingModes, lightControls, generalSettings, colourBiz, debug, waveBiz;
 
 
 
 function setup() {
-  createCanvas(windowWidth, windowHeight, WEBGL);
-  
+  // createCanvas(windowWidth, windowHeight, WEBGL);
+  createCanvas(1080, 1080, WEBGL);
+  pixelDensity(1);
   
   gui = new lil.GUI();
   params = {
-    nFieldPoints: 500,
-    nControlPoints: 2,
-    noiseProgRate: 0.001,
-    sep: 10,
+    nFieldPoints: 3000,
+    nControlPoints: 5,
+    noiseProgRate: 0.005,
+    sep: 25,
     amp: 20,
-    showControlPoints: true,
+    showControlPoints: false,
     phase: 0,
-    cycles: 3
+    cycles: 5,
+    sizeThreshold: 0,
+    bgColour: color(0),
+    fieldColour: color('#f94144'),
+    ambientLightOn: false,
+    pointLightZHeight: height/2,
+    pointLightOn: true,
+    decayRate: 0,
+    distanceMappingFunction: decayingCosineWave
   }
 
-  gui.add(params, 'nFieldPoints', 100, 5000, 1)
-     .onChange(value => pts = genSquarePtPlane(value));
-  gui.add(params, 'nControlPoints', 1, 20, 1)
-     .onChange(value => controlPoints = generateControlPoints(value));
-  gui.add(params, 'noiseProgRate', 0.0001, 0.01);
-  gui.add(params, 'sep', 1, width/2).onChange(value => pts = genSquarePtPlane(value));
-  gui.add(params, 'amp', 5, 500);
-  gui.add(params, 'phase', 0, TWO_PI);
-  gui.add(params, 'cycles', 1, 10, 1);
-  gui.add(params, 'showControlPoints');
+  // ---------------- GENERAL SETTINGS --------------- //
+  generalSettings = gui.addFolder('General Settings');
+  generalSettings.add(params, 'nFieldPoints', 100, 5000, 1).onChange(value => pts = genSquarePtPlane(value));
+  generalSettings.add(params, 'nControlPoints', 1, 20, 1).onChange(value => controlPoints = generateControlPoints(value));
+  generalSettings.add(params, 'noiseProgRate', 0.0001, 0.01);
+  generalSettings.add(params, 'sep', 1, width/50).onChange(value => pts = genSquarePtPlane(params.nFieldPoints));
+
+  // ---------------- WAvE BIZ --------------- //
+  waveBiz = gui.addFolder('Wave Biz');
+  waveBiz.add(params, 'amp', 1, min(width,height)/25);
+  waveBiz.add(params, 'phase', 0, TWO_PI);
+  waveBiz.add(params, 'cycles', 1, 10, 1);
+  waveBiz.add(params, 'sizeThreshold', 0, params.amp);
+
+  // ---------------- COLOUR BIZ --------------- //
+  colourBiz = gui.addFolder('Colour Biz');
+  colourBiz.add(params, 'bgColour', {'black':color(0), 'white': color(255)});
+  colourBiz.add(params,'fieldColour', {
+    "Imperial red":color("#f94144"),
+    "Orange (Crayola)":color("#f3722c"),
+    "Carrot orange":color("#f8961e"),
+    "Coral":color("#f9844a"),
+    "Saffron":color("#f9c74f"),
+    "Pistachio":color("#90be6d"),
+    "Zomp":color("#43aa8b"),
+    "Dark cyan":color("#4d908e"),
+    "Payne's gray":color("#577590"),
+    "Cerulean":color("#277da1")
+  });
   
+  //------------- LIGHT CONTROLS --------------//
+  lightControls = gui.addFolder('Light Controls');
+  lightControls.add(params, 'ambientLightOn');
+  lightControls.add(params, 'pointLightOn');
+  lightControls.add(params, 'pointLightZHeight', 0, 2*height);
+
+  //------------- DISTANCE MAPPING MODES--------------//
+  distanceMappingModes = gui.addFolder('Distance Mapping Modes');
+  distanceMappingModes.add(params, 'decayRate', 0, 1);
+  distanceMappingModes.add(params, 'distanceMappingFunction', {'cosineWave' : cosineWave, 'decayingCosineWave' : decayingCosineWave});
+  
+  //------------- DEBUG --------------//
+  debug = gui.addFolder("Debug");
+  debug.add(params, 'showControlPoints');
 
   controlPoints = generateControlPoints(params.nControlPoints);
-
   pts = genSquarePtPlane(params.nFieldPoints);
+
   noStroke();
-  
 }
 
 function draw() {
-  background(220);
-  fill(255,0,0);
+  background(params.bgColour);
+  fill(params.fieldColour);
   noStroke();
 
-  pointLight(255, 255, 255, 0,0,height/2);
+  // -------------- LIGHTS ---------------- //
+  if(params.pointLightOn) pointLight(255, 255, 255, 0, 0, params.pointLightZHeight);
+  if(params.ambientLightOn) ambientLight(255, 255, 255);
+  
+  // CAMERA
+
+
+  // ACTION
   for(let cpt of controlPoints){
     cpt.update();
-    cpt.show();
+    if(params.showControlPoints) cpt.show();
   }
 
   for(let pt of pts){
@@ -68,15 +135,18 @@ function draw() {
     let r = 0;
     for(let cntrlPt of controlPoints){
       let d = map(p5.Vector.dist(cntrlPt.pos, pt), 0, params.sep*sqrt(params.nFieldPoints), 0, TWO_PI);
-      r += params.amp * sin(params.cycles*d + params.phase);
+      r += params.distanceMappingFunction(d);
     }
-    sphere(r);
+    if(abs(r) > params.sizeThreshold) sphere(r);
     pop();
   }
 
   orbitControl();
 }
 
+const cosineWave = (d) => {return params.amp * sin(params.cycles*d + params.phase)}
+
+const decayingCosineWave = (d) => {return params.amp * exp(-params.decayRate*d) * cos(params.cycles*d + params.phase)};
 
 function generateControlPoints(n){
   let cpts = [];
@@ -86,6 +156,10 @@ function generateControlPoints(n){
   return cpts;
 }
 
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
 
 function genSquarePtPlane(n){
   let points = [];
@@ -112,12 +186,11 @@ class ControlPoint{
   }
 
   show(){
-    if(params.showControlPoints){
-      fill(255);
-      push();
-        translate(this.pos.x, this.pos.y, this.pos.z);
-        sphere(min(width, height)/20);
-      pop();
-    }
+    fill(255);
+    push();
+      translate(this.pos.x, this.pos.y, this.pos.z);
+      sphere(min(width, height)/20);
+    pop();
+    
   }
 }
